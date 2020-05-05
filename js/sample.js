f8455e592fdb884bbf60ddea753c1a5f57757a45
msdysk <masuda.yoshiki.idh@gmail.com>
2020-05-05 15:38:34 +0900

    固定ページ、詳細ページのスマホ表示の体裁を修正

--- js/sample.js
+++
@@ -1,8 +0,0 @@
-jQuery(function(){
-    jQuery(".menu").on("click",function(){
-      jQuery(".sp-menu").slideToggle();
-      jQuery(".icon-menu").toggle();
-      jQuery(".icon-close").toggle();
-      return false;
-    })
-  })
