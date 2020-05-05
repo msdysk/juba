2799589ea1532983c40b3a5d8cac5a5edf48ca2d
msdysk <masuda.yoshiki.idh@gmail.com>
2020-05-05 16:03:03 +0900

    投稿ページのデザインを修正

--- comments.php
+++ comments.php
@@ -1,11 +1,14 @@
+<p><h3>コメント</h3></p>
 <ol class="comment-list">
+<?php if( have_comments() ){ ?>
   <?php wp_list_comments(); ?>
 </ol>
+<?php } ?>
 
 <div class="comment-form">
   <?php 
     $comment_args = array(
-      'title_reply' => 'コメントはこちらから',
+      'title_reply' => 'コメントを残す',
       'label_submit' => 'コメントを送信する',
     );    
     comment_form($comment_args); ?>
