0b5c44f36277c4a6cf3e92e4ce82460d0fca7443
katsuya-2977 <katsuya1308@gmail.com>
2020-05-07 21:13:01 +0900

    footer h3 3・９行目

--- footer.php
+++ footer.php
@@ -1,13 +1,11 @@
 <footer>
   <ul class="links-social">
-  <h3>週末起業塾SNS</h3>
-        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-fb.png" alt="Facebook" class="icon">Facebook</a></li>
-        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-tw.png" alt="Twitter" class="icon">Twitteer</a></li>
-        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-ins.png" alt="Instagram" class="icon">Instagram</a></li>
+        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-fb2.png" alt="Facebook" class="icon">Facebook</a></li>
+        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-tw2.png" alt="Twitter" class="icon">Twitteer</a></li>
+        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-insg.png" alt="Instagram" class="icon">Instagram</a></li>
       </ul>
   <ul class="links-main-pages">
-  <h3>所在地</h3>
-        <li><a href="#">東京都港区赤坂1-○-○</a></li>
+        <li><a href="#">所在地：東京都港区赤坂1-○-○</a></li>
       </ul>
 </footer>
 
