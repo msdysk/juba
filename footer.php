77d88168abc29069bd2739d79cb8286367953d4a
katsuya-2977 <katsuya1308@gmail.com>
2020-05-07 20:46:07 +0900

    footer変更

--- footer.php
+++ footer.php
@@ -1,5 +1,14 @@
 <footer>
-  
+  <ul class="links-social">
+  <h3>週末起業塾SNS</h3>
+        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-fb.png" alt="Facebook" class="icon">Facebook</a></li>
+        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-tw.png" alt="Twitter" class="icon">Twitteer</a></li>
+        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-ins.png" alt="Instagram" class="icon">Instagram</a></li>
+      </ul>
+  <ul class="links-main-pages">
+  <h3>所在地</h3>
+        <li><a href="#">東京都港区赤坂1-○-○</a></li>
+      </ul>
 </footer>
 
 <?php wp_footer(); ?>
